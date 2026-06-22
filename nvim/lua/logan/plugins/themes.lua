2d6003d32dcfbb6a11ca18ed55044214e8b7cf65
Logan <loganlugez8@gmail.com>
2026-06-22 12:54:49 +0200

    ok

--- nvim/lua/logan/plugins/themes.lua
+++
@@ -1,8 +0,0 @@
-return {
-	'ThorstenRhau/token',
-	lazy = false,      -- charger au démarrage
-	priority = 1000,   -- charger avant les autres plugins
-	config = function()
-	vim.cmd.colorscheme('token')
-	end,
-}
