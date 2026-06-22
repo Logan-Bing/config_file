92c14a7acd36e243be6c7bc9dcabe831ef3b06c7
Logan <loganlugez8@gmail.com>
2026-06-22 17:27:12 +0200

    ok

--- nvim/lua/logan/config/keymaps.lua
+++ nvim/lua/logan/config/keymaps.lua
@@ -2,7 +2,7 @@ local keymap = vim.keymap.set
 
 keymap("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
 keymap("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
-keymap("n", "<leader>a", "<cmd>qa<cr>", { desc = "Quit session" })
+keymap("n", "<leader>a", "<cmd>wqa<cr>", { desc = "Quit session" })
 keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 keymap("n", "<leader>x", "<cmd>lua vim.diagnostic.open_float()<CR>")
 keymap("n", "<leader>p", "<C-6>", { desc = "alterner entre deux files"})
@@ -12,6 +12,10 @@ keymap("n", "<leader>k", ":move .-2<CR>==")
 keymap("n", "<leader>j", ":move .+1<CR>==")
 keymap("v", "<leader>k", ":move '<-2<CR>==")
 keymap("v", "<leader>j", ":move '>+1<CR>==")
+keymap("n", "<leader>H", ":vertical resize +10<CR>")
+keymap("n", "<leader>L", ":vertical resize -10<CR>")
+keymap("n", "<leader>K", ":horizontal resize +10<CR>")
+keymap("n", "<leader>J", ":horizontal resize -10<CR>")
 
 -- split
 keymap("n", "<leader>|", ":vsplit<CR>")
